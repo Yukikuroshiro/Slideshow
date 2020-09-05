78e16309db6467bde6e72bc0d4a52360300f75d2
Gabriel Mochiute <Yukikuroshiro@users.noreply.github.com>
2020-09-05 20:32:55 -0300

    finalizao do exercicio

--- slideShow.js
+++ slideShow.js
@@ -38,5 +38,45 @@ const next = () => {
     items = document.querySelectorAll('.item');
 }
 
+const lista = document.querySelector('#listaQuantidade');
+
+let listaMenu = [];
+
+const createList = () => {
+    for (let index = 0; index < images.length; index++) {
+        listaMenu[index] = "";
+    }
+}
+createList();
+
+const loadList = () => {
+    for (let index = 0; index < images.length; index++) {
+        lista.innerHTML += `
+        <li class="guideList">
+            ${listaMenu[index]}
+        </li>
+        `
+    }
+}
+loadList();
+
+let guideList = document.querySelectorAll('.guideList');
+guideList[0].id = "menuSelecionado";
+
+
+const listPrevious = () => {
+    lista.appendChild(guideList[0]);
+    guideList = document.querySelectorAll('.guideList');
+}
+
+const listNext = () => {
+    const lastList = guideList[guideList.length - 1];
+    lista.insertBefore(lastList, guideList[0]);
+    guideList = document.querySelectorAll('.guideList');
+}
+
 document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#previous').addEventListener('click', listPrevious);
+
 document.querySelector('#next').addEventListener('click', next);
+document.querySelector('#next').addEventListener('click', listNext);
